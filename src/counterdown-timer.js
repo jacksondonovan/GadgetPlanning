02dc3626e893868f26b491feeb556215b043ae22
Jackson Donovan <iinventedtwitter@gmail.com>
2018-04-18 15:35:36 -0600

    indiegogo timer loads instantly now

--- src/counterdown-timer.js
+++ src/counterdown-timer.js
@@ -7,10 +7,10 @@ var x = setInterval(function() {
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+  document.getElementById("demo").innerHTML = days + " Days " + hours + " Hours ";
+  document.getElementById("lowerdemo").innerHTML = minutes + " Minutes " + seconds + " Seconds ";
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+    document.getElementById("demo").innerHTML = "LAUNCHED!";
   }
-}, 1000);
+}, 0000);
